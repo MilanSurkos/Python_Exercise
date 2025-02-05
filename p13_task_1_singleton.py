f8f94117bd70b4ac80c8f07c15f1b5b112470c57
Milan <surkos.milan@gmail.com>
2025-02-05 17:43:15 +0100

    docs: update

--- p13_task_1_singleton.py
+++ p13_task_1_singleton.py
@@ -4,3 +4,32 @@ Pomocí vzoru Singleton vytvořte jednoduchou třídu loggeru,
 která do souboru zapíše řetězce a datum a čas.
 Použijte vnořenou třídu, která shromažďuje funkce instance
 a přetížení metody new."""
+
+
+
+import os
+from datetime import datetime
+
+class Logger:
+    # Vnořená třída pro uchování instance
+    class __Logger:
+        def __init__(self):
+            self.filename = "log.txt"
+
+        def log(self, message):
+            with open(self.filename, "a") as file:
+                timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                file.write(f"{timestamp} - {message}\n")
+
+    instance = None
+
+    def __new__(cls):
+        if not cls.instance:
+            cls.instance = cls.__Logger()
+        return cls.instance
+
+# Příklad použití
+if __name__ == "__main__":
+    logger = Logger()
+    logger.log("Toto je prvá správa.")
+    logger.log("Toto je druhá správa.")
